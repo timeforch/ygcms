feaeed6f482893e0c6b23f9cb4cbb674e7bc6c68
minxr <minxiaorong@126.com>
2013-06-08 13:56:02 +0800

    添加了extjs4.2.1的包

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,1 @@
+// the enter of app
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cms::Application.config.secret_token = '25b64e0698e888c4668936ad50419eb0c8ac2df8e0cdfc2bc7111929d5ca88f8b1e93fba68a3b8e9a05048012d2f23ce13707ebce1409346a6db82989c19ffea'

dced2a7f63332216842b30d76a44e767bd3dce5b
minxr <minxiaorong@126.com>
2013-06-28 16:03:38 +0800

    add init data to seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#attr_accessible :enabled, :login_name, :password, :phone_number, :user_name
+User.create({user_name:'minxiaorong',
+			 login_name:'min.xr',
+			 password:'pwd',
+			 phone_number:'13444444444',
+			 enabled:false})
+User.create({user_name:'tanfeng',
+			 login_name:'tan.f',
+			 password:'pwd',
+			 phone_number:'13444444444',
+			 enabled:false})

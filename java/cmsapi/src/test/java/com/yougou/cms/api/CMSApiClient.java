f2fe5304a9cd6d951bc12803a2515ab0d29a2e0c
minxr <minxiaorong@126.com>
2013-07-02 16:39:19 +0800

    add java client demo

---
+++ java/cmsapi/src/test/java/com/yougou/cms/api/CMSApiClient.java
@@ -0,0 +1,38 @@
+package com.yougou.cms.api;
+
+import com.yougou.cms.vo.UserVo;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TFastFramedTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: minxr
+ * Date: 7/2/13
+ * Time: 2:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CMSApiClient {
+    public static void main(String[] args) throws TException {
+        //设置调用的服务器为本地，端口为7911
+        TTransport transport = new TSocket("localhost", 9090);
+        transport =  new TFastFramedTransport(transport);
+        //设置传输协议为TBinaryProtocol
+        TProtocol protocol = new TBinaryProtocol(transport);
+//        Hello.Client client = new Hello.Client(protocol);
+        transport.open();
+
+        ICMSApi.Client client = new ICMSApi.Client(protocol);
+        System.out.println(client.getUserCount());
+        List<UserVo> userList = client.getAllUsers();
+        for (UserVo u:userList){
+            System.out.println(u.getUser_name());
+        }
+        transport.close();
+    }
+}

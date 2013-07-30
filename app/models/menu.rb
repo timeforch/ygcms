7bf26211dfb7848bee55a0eb5acd0c1dad539899
minxr <minxiaorong@126.com>
2013-07-30 14:27:40 +0800

    add menu's validator

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,6 @@
+#encoding:utf-8
 class Menu < ActiveRecord::Base
   self.primary_key = "sid"
   attr_accessible :click_url, :display, :name, :parent_path, :sid
+  validates :sid,presence:{message:"主键不能为空！"}
 end

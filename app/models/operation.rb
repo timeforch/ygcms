c4ff6e8c7f7bf851e26152fc7fb5bf377137955e
minxr <minxiaorong@126.com>
2013-07-30 15:20:51 +0800

    add validate

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,4 +1,8 @@
+#encoding:utf-8
 class Operation < ActiveRecord::Base
   self.primary_key = "sid"
   attr_accessible :action_name, :sid, :url
+  validates :sid,presence:{message:"主键不能为空！"}
+  validates :action_name,length: {minimum: 2,maximum: 32,message:"操作名称应大于2个字符，小于30个字符"}
+  validates :url,presence: {message:"url不能为空"}
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,9 @@
+#encoding:utf-8
 class Role < ActiveRecord::Base
   self.primary_key = "sid"
   attr_accessible :name ,:sid
+  validates :sid,presence:{message:"主键不能为空！"}
+  validates :name,presence: {message:"角色名称不能为空"}
   has_many :user_roles
   has_many :users, through: :user_roles
 end

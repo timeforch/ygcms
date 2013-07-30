4a9bbef256e558821a26a030514c81950ead5746
minxr <minxiaorong@126.com>
2013-07-30 09:20:30 +0800

    update server

--- app/services/server.rb
+++ app/services/server.rb
@@ -4,21 +4,23 @@ require 'cms_api'
 require 'cms_api_types'
 require 'cms_api_constants'
 require File.expand_path('../cms_api_service', __FILE__)
+def run_server(port)
+  begin
+    handler = CmsApiService.new
 
-begin
-  handler = CmsApiService.new
-
-  processor = ICMSApi::Processor.new(handler)
-  transport = Thrift::ServerSocket.new('0.0.0.0', 9090)
+    processor = ICMSApi::Processor.new(handler)
+    transport = Thrift::ServerSocket.new('0.0.0.0', port)
 #transport = Thrift::NonblockingServer.new(transport)
-  tFactory = Thrift::FramedTransportFactory.new()
-  pFactory = Thrift::BinaryProtocolFactory.new()
-  server = Thrift::NonblockingServer.new(processor, transport, tFactory, pFactory)
+    tFactory = Thrift::FramedTransportFactory.new()
+    pFactory = Thrift::BinaryProtocolFactory.new()
+    server = Thrift::NonblockingServer.new(processor, transport, tFactory, pFactory)
 
-  puts "Starting the server..."
-  server.serve()
-  puts "done."
-rescue
-  puts "error:#{$!} at:#{$@}"
+    puts "Starting the server..."
+    server.serve()
+    puts "done."
+  rescue
+    puts "error:#{$!} at:#{$@}"
+  end
+
 end
-
+run_server(ARGV[0])
